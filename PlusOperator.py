ef923d0e7148939055d3e139f7d3136c0846819a
Pierce O'Neill <pierce.oneill@wpengine.com>
2024-07-16 16:55:28 +0100

    Addition of PlusOperator.py

---
+++ PlusOperator.py
@@ -0,0 +1,10 @@
+x=5
+y=3
+z="ABC"
+print("Hello "+z)       # Hello ABC
+print("Hello "+"XYZ")   # Hello XYZ
+print(x+y)              # 8 → Addition
+print(x+z)              # Error
+
+a="10"
+print(x+a)              # Error
