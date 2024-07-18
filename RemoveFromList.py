914454d5f7db41ad72c90dcf2f6801ebdb19028d
Pierce O'Neill <pierceoneillky@gmail.com>
2024-07-18 22:09:34 +0100

    "addition of RemoveFromList.py"

---
+++ RemoveFromList.py
@@ -0,0 +1,7 @@
+"""
+Remove Item
+There are several methods to remove items frin list1 list:
+1. remove () - Removes Specific Item
+2. pop(indexNo) - Removes the specified index
+3. pop() - Removes the last item if index is not specified.
+"""
