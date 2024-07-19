e630c1e4be33f8a59752393ceaa3bed38fff98ce
Pierce O'Neill <pierceoneillky@gmail.com>
2024-07-19 14:11:55 +0100

    "updating comments on RemoveFromList"

--- RemoveFromList.py
+++ RemoveFromList.py
@@ -4,4 +4,7 @@ There are several methods to remove items frin list1 list:
 1. remove () - Removes Specific Item
 2. pop(indexNo) - Removes the specified index
 3. pop() - Removes the last item if index is not specified.
+4. del keyword with given index number deletes specific element
+5. del keyword without index no. deletes whole list
+6 clear() - empties the list, will not delete the list
 """
