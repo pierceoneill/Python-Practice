e0ca0ea0bf3310f5510578bd4af5e04a1150b71a
Pierce O'Neill <pierceoneillky@gmail.com>
2024-07-30 16:48:24 +0100

    using list() constructor

--- CopyList.py
+++ CopyList.py
@@ -9,3 +9,12 @@ There are ways to make list copy. list methods:
 
 # Define a list
 list1 = ["A", "B", "C", "D", "E"]
+print(list1)
+
+# using copy()
+list2 = list1.copy()
+print(list2)
+
+# using list() constructor
+list3 = list(list1)
+print(list3)
